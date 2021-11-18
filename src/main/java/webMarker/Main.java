d7219fb697513a6709247802299557b6b8c54501
Oleksandr Haleta <alexhaleta99@gmail.com>
2021-11-18 17:59:10 +0200

    feat: setting view products

--- src/main/java/webMarker/Main.java
+++ src/main/java/webMarker/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.addServlet(new ServletHolder(new AllRequestsServlet()), "/*");
+        context.addServlet(new ServletHolder(new HomeServlet()), "/");
         context.addServlet(new ServletHolder(new ProductsServlet()), "/products");
         context.addServlet(new ServletHolder(new ItemServlet()), "/product");
         context.addServlet(new ServletHolder(new AddServlet()), "/products/add");
